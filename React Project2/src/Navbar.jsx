f11eb5540c955acae54e0934475ddb86a33f9821
Betsemfon Udom <betsemfonu@gmail.com>
2024-07-16 11:09:18 +0100

    React images

--- React Project2/src/Navbar.jsx
+++ React Project2/src/Navbar.jsx
@@ -12,11 +12,13 @@ function NavBar(){
                         <li><a>About Us</a></li>
                         <li><a>Contact</a></li>
                         <li><a>Blog</a></li>
+                        <button>+ Post Listing</button>
                     </ul>
                   
                 </nav> 
                 <div className="image-container">
                 <img  className="Banner-image" src={profilePicture} alt="profile picture"/>
+                <p></p>
             </div>
     </header>
                 
